42b9f5e55bd17ac14e20747294bccacafb286d46
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-16 10:24:34 +0200

    list#3

--- src/main/java/com/spring/java8/list/ListDemo.java
+++ src/main/java/com/spring/java8/list/ListDemo.java
@@ -6,22 +6,34 @@ import java.util.List;
 public class ListDemo {
 
     public static void main(String[] args) {
-        List<Integer> primeNumbers= new ArrayList<>();
-        primeNumbers.add(2);
-        primeNumbers.add(3);
-        primeNumbers.add(5);
-        primeNumbers.add(7);
-        primeNumbers.add(11);
+        List<String> programmingLangs= new ArrayList<>();
+        System.out.println("Are Programming Langs Is Empty?: "+ programmingLangs.isEmpty());
+        programmingLangs.add("Java");
+        programmingLangs.add("PHP");
+        programmingLangs.add("C#");
+        programmingLangs.add("C++");
+        System.out.println("SIZE: "+ programmingLangs.size());
 
-        List<Integer> tenPrimenumbers= new ArrayList<>(primeNumbers);
-        List<Integer> fivePrimeNumbers = new ArrayList<>();
-        fivePrimeNumbers.add(13);
-        fivePrimeNumbers.add(17);
-        fivePrimeNumbers.add(19);
-        fivePrimeNumbers.add(23);
-        fivePrimeNumbers.add(29);
-        tenPrimenumbers.addAll(fivePrimeNumbers);
-        tenPrimenumbers.forEach((e)-> System.out.println(e));
+        System.out.println("GET INDEX: "+ programmingLangs.get(1));
+        programmingLangs.set(3, "Spring Boot");
+        programmingLangs.forEach((e)-> System.out.println(e));
+
+//        List<Integer> primeNumbers= new ArrayList<>();
+//        primeNumbers.add(2);
+//        primeNumbers.add(3);
+//        primeNumbers.add(5);
+//        primeNumbers.add(7);
+//        primeNumbers.add(11);
+//
+//        List<Integer> tenPrimenumbers= new ArrayList<>(primeNumbers);
+//        List<Integer> fivePrimeNumbers = new ArrayList<>();
+//        fivePrimeNumbers.add(13);
+//        fivePrimeNumbers.add(17);
+//        fivePrimeNumbers.add(19);
+//        fivePrimeNumbers.add(23);
+//        fivePrimeNumbers.add(29);
+//        tenPrimenumbers.addAll(fivePrimeNumbers);
+//        tenPrimenumbers.forEach((e)-> System.out.println(e));
 
 
 //        List<String> list = new ArrayList<>();
