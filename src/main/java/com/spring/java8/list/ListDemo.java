422b03777f2f0d76a5a7624177fd2770e8cf7bf2
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-16 10:10:26 +0200

    list#2

--- src/main/java/com/spring/java8/list/ListDemo.java
+++ src/main/java/com/spring/java8/list/ListDemo.java
@@ -6,26 +6,44 @@ import java.util.List;
 public class ListDemo {
 
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("Ahmed");
-        list.add("Ahmed");
-        list.add("Mohame");
-        list.add("Ali");
-        System.out.println(list);
-        list.forEach((e)->{
-            System.out.println(e);
-        });
-        list.add(null);
-        list.add(null);
-        System.out.println(list);
-        System.out.println("=============");
-        List<Integer> list1 = new ArrayList<>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(4);
-        list1.add(3);
-        System.out.println(list1);
-        System.out.println(list1.get(2));
-        System.out.println(list1.get(0));
+        List<Integer> primeNumbers= new ArrayList<>();
+        primeNumbers.add(2);
+        primeNumbers.add(3);
+        primeNumbers.add(5);
+        primeNumbers.add(7);
+        primeNumbers.add(11);
+
+        List<Integer> tenPrimenumbers= new ArrayList<>(primeNumbers);
+        List<Integer> fivePrimeNumbers = new ArrayList<>();
+        fivePrimeNumbers.add(13);
+        fivePrimeNumbers.add(17);
+        fivePrimeNumbers.add(19);
+        fivePrimeNumbers.add(23);
+        fivePrimeNumbers.add(29);
+        tenPrimenumbers.addAll(fivePrimeNumbers);
+        tenPrimenumbers.forEach((e)-> System.out.println(e));
+
+
+//        List<String> list = new ArrayList<>();
+//        list.add("Ahmed");
+//        list.add("Ahmed");
+//        list.add("Mohame");
+//        list.add("Ali");
+//        System.out.println(list);
+//        list.forEach((e)->{
+//            System.out.println(e);
+//        });
+//        list.add(null);
+//        list.add(null);
+//        System.out.println(list);
+//        System.out.println("=============");
+//        List<Integer> list1 = new ArrayList<>();
+//        list1.add(1);
+//        list1.add(2);
+//        list1.add(4);
+//        list1.add(3);
+//        System.out.println(list1);
+//        System.out.println(list1.get(2));
+//        System.out.println(list1.get(0));
     }
 }
