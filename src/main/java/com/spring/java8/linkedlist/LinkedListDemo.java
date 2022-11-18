07eb4488cb100e3106750d4ce95ae7844e1337a6
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-18 08:02:43 +0200

    LinkedList#2

--- src/main/java/com/spring/java8/linkedlist/LinkedListDemo.java
+++ src/main/java/com/spring/java8/linkedlist/LinkedListDemo.java
@@ -7,15 +7,26 @@ public class LinkedListDemo {
 
     public static void main(String[] args) {
         LinkedList<String> programmingLangs = new LinkedList<>();
+        programmingLangs.add("HtmL");
         programmingLangs.add("Java");
         programmingLangs.add("PHP");
         programmingLangs.add("Laravel");
         programmingLangs.add("JS");
+        programmingLangs.add("NodeJs");
+        programmingLangs.add("Ruby");
         programmingLangs.add("CSS");
+        System.out.println(programmingLangs.removeFirst());
+        System.out.println(programmingLangs.removeLast());
+        System.out.println(programmingLangs.remove(4));
+        programmingLangs.remove("Ruby");
+        System.out.println("==================");
         programmingLangs.stream().forEach(programmingLang-> System.out.println(programmingLang));
-        System.out.println("FIrst Element: "+ programmingLangs.getFirst());
-        System.out.println("Last Element: "+ programmingLangs.getLast());
-        System.out.println("Get Index: "+ programmingLangs.get(3));
+
+//        programmingLangs.stream().forEach(programmingLang-> System.out.println(programmingLang));
+//        System.out.println("FIrst Element: "+ programmingLangs.getFirst());
+//        System.out.println("Last Element: "+ programmingLangs.getLast());
+//        System.out.println("Get Index: "+ programmingLangs.get(3));
+
 
 
 //        LinkedList<String> programmingLangs = new LinkedList<>();
