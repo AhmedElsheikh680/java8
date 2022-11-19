066698ae5c5a4a391617191ce73e067d00541773
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-19 08:26:43 +0200

    copy list to another list

---
+++ src/main/java/com/spring/java8/list/CopyListToAnotherList.java
@@ -0,0 +1,31 @@
+package com.spring.java8.list;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CopyListToAnotherList {
+
+    public static void main(String[] args) {
+        List<String> courses = new ArrayList<>(Arrays.asList("Java", "Js", "C#", "Ruby"));
+        courses.stream().forEach(course-> System.out.println(course));
+//        List<String> copyCourses = new ArrayList<>(courses);
+//        System.out.println("==========");
+//        copyCourses.forEach(copyCourse-> System.out.println(copyCourse));
+        System.out.println("-------------------------");
+//        List<String> copyCourses = new ArrayList<>();
+//        copyCourses.addAll(courses);
+//        copyCourses.stream().forEach(copyCourse-> System.out.println(copyCourse));
+
+//        List<String> copyCourses = new ArrayList<>();
+//        Collections.copy(copyCourses, copyCourses);
+//        courses.stream().forEach(copyCourse-> System.out.println(copyCourse));
+
+//        List<String> copyCourses = courses.stream().collect(Collectors.toList());
+//        System.out.println(copyCourses);
+
+        List<String> copyCourses =List.copyOf(courses);
+        copyCourses.stream().forEach(copyCourse-> System.out.println(copyCourse));
+
+
+    }
+}
--- src/main/java/com/spring/java8/map/HashMapDemo.java
+++ src/main/java/com/spring/java8/map/HashMapDemo.java
@@ -12,34 +12,36 @@ public class HashMapDemo {
         courses.put(2, "C++");
         courses.put(3, "Java");
         courses.put(4, "PHP");
-        Iterator<Map.Entry<Integer, String>> iterator = courses.entrySet().iterator();
-        while (iterator.hasNext()){
-            Map.Entry<Integer, String> entry = iterator.next();
-            System.out.print(entry.getKey()+ " - ");
-            System.out.println(entry.getValue());
-        }
 
-        Iterator<Integer> iterator = courses.keySet().iterator();
-        while (iterator.hasNext()){
-            Integer key = iterator.next();
-            System.out.println(key);
-            System.out.println(courses.get(key));
-        }
+//        Iterator<Map.Entry<Integer, String>> iterator = courses.entrySet().iterator();
+//        while (iterator.hasNext()){
+//            Map.Entry<Integer, String> entry = iterator.next();
+//            System.out.print(entry.getKey()+ " - ");
+//            System.out.println(entry.getValue());
+//        }
 
-        for (Map.Entry<Integer, String> entry: courses.entrySet()){
-            System.out.print(entry.getKey()+" ");
-            System.out.println(entry.getValue());
-        }
+//        Iterator<Integer> iterator = courses.keySet().iterator();
+//        while (iterator.hasNext()){
+//            Integer key = iterator.next();
+//            System.out.println(key);
+//            System.out.println(courses.get(key));
+//        }
 
-        courses.forEach((key, value)->{
-            System.out.print(key+ "=> ");
-            System.out.println(value);
-        });
+//        for (Map.Entry<Integer, String> entry: courses.entrySet()){
+//            System.out.print(entry.getKey()+" ");
+//            System.out.println(entry.getValue());
+//        }
 
-        courses.entrySet().stream().forEach((entry)-> {
-            System.out.print(entry.getKey()+" ");
-            System.out.println(entry.getValue());
-        });
+
+//        courses.forEach((key, value)->{
+//            System.out.print(key+ "=> ");
+//            System.out.println(value);
+//        });
+
+//        courses.entrySet().stream().forEach((entry)-> {
+//            System.out.print(entry.getKey()+" ");
+//            System.out.println(entry.getValue());
+//        });
 
 
     }
