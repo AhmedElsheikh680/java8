2c8da39cc280302c89d7671572e8607e9af08ce8
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-22 14:42:19 +0200

    min, max, count, findFirst, findAny

--- src/main/java/com/spring/java8/stream/MapDemo.java
+++ src/main/java/com/spring/java8/stream/MapDemo.java
@@ -4,23 +4,51 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapDemo {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User(1, "Ahmed", "123", "a@a.com"));
-        users.add(new User(2, "Gamal", "123", "g@g.com"));
-        users.add(new User(3, "Mohamed", "123", "m@m.com"));
-        users.add(new User(4, "Ramy", "123", "r@r.com"));
-        List<UserDTO> userDTOS = new ArrayList<>();
-        for (User user: users){
-            userDTOS.add(new UserDTO(user.getId(), user.getName(), user.getEmail()));
-        }
-        userDTOS.stream().forEach(System.out::println);
 
+        Stream<Integer> nums = Stream.of(10,20,30,40,100);
+        System.out.println(nums.count());
+        Integer minNumber = Stream.of(10,20,30,40,100).min(Comparator.comparing(Integer::valueOf)).get();
+        System.out.println("Min: "+ minNumber);
+        Integer max= Stream.of(10,20,30,40,100).max(Comparator.comparing(Integer::valueOf)).get();
+        System.out.println("Max: "+max);
+
+//        List<Integer> list = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+//        Optional<Integer> optional = list.stream().findAny();
+//        if (optional.isPresent()){
+//            System.out.println(optional.get());
+//        } else {
+//            System.out.println("Stream Is Empty");
+//        }
+//        Optional<Integer> optional = list.stream().findFirst();
+//        if (optional.isPresent()){
+//            System.out.println(optional.get());
+//        } else {
+//            System.out.println("element not Found");
+//        }
     }
+//        List<User> users = new ArrayList<>();
+//        users.add(new User(1, "Ahmed", "123", "a@a.com"));
+//        users.add(new User(2, "Gamal", "123", "g@g.com"));
+//        users.add(new User(3, "Mohamed", "123", "m@m.com"));
+//        users.add(new User(4, "Ramy", "123", "r@r.com"));
+//        List<UserDTO> userDTOS = new ArrayList<>();
+//        for (User user: users){
+//            userDTOS.add(new UserDTO(user.getId(), user.getName(), user.getEmail()));
+//        }
+//        userDTOS.stream().forEach(System.out::println);
+//        users.stream().map((User user)-> new UserDTO(user.getId(), user.getName(), user.getEmail()))
+//                .collect(Collectors.toList()).forEach(System.out::println);
+
+//        users.stream().map((User user)-> new UserDTO(user.getId(), user.getName(), user.getEmail()))
+//                .collect(Collectors.toList()).forEach((userDTO -> System.out.println(userDTO)));
+//
+//    }
 }
 @Data
 @NoArgsConstructor
