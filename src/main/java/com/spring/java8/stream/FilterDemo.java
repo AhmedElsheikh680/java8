851e36220b7245f3d847866488910575366e5fe8
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-22 12:47:25 +0200

    stream filter sorted

--- src/main/java/com/spring/java8/stream/FilterDemo.java
+++ src/main/java/com/spring/java8/stream/FilterDemo.java
@@ -5,31 +5,53 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FilterDemo {
 
     public static void main(String[] args) {
-    List<Product> products = new ArrayList<>();
-    for (Product product: products){
-        if (product.getPrice() > 2500f){
-            products.add(product);
-        }
-    }
+//    List<Product> products = new ArrayList<>();
+//    for (Product product: products){
+//        if (product.getPrice() > 2500f){
+//            products.add(product);
+//        }
+//    }
 //    getProducts().stream().forEach(System.out::println);
 //        for (Product product: getProducts()){
 //            System.out.println(product);
 //        }
-        getProducts().stream().filter((product)->product.getPrice()> 2500f).collect(Collectors.toList()).forEach(System.out::println);
+//        getProducts().stream().filter((product)->product.getPrice()> 2500f)
+//                .collect(Collectors.toList()).forEach(System.out::println);
+
+//        getProducts().stream().
+//                sorted((o1, o2) ->o2.getName().compareTo(o1.getName()))
+//                .collect(Collectors.toList()).forEach(System.out::println);
+
+//        getProducts().stream().sorted(new Comparator<Product>() {
+//            @Override
+//            public int compare(Product o1, Product o2) {
+//                return (int) (o2.getPrice() - o1.getPrice());
+//            }
+//        }).collect(Collectors.toList()).forEach(System.out::println);
+
+//        getProducts().stream().sorted((o1, o2) -> (int) (o1.getPrice() - o2.getPrice()))
+//                .collect(Collectors.toList()).forEach(System.out::println);
+
+//        getProducts().stream().sorted(Comparator.comparingDouble(Product::getPrice))
+//                .collect(Collectors.toList()).forEach(System.out::println);
+
+        getProducts().stream().sorted(Comparator.comparingDouble(Product::getPrice).reversed())
+                .collect(Collectors.toList()).forEach(System.out::println);
     }
 
     public static List<Product> getProducts(){
         List<Product> products =  new ArrayList<>();
-        products.add(new Product(1, "Ahmed", 2500f));
-        products.add(new Product(2, "Mohamed", 3000f));
-        products.add(new Product(3, "Ali", 28000f));
-        products.add(new Product(4, "Hany", 9000f));
+        products.add(new Product(1, "Ahmed", 2500.0));
+        products.add(new Product(2, "Zeyad", 3000.0));
+        products.add(new Product(3, "Kamal", 28000.0));
+        products.add(new Product(4, "Gamal", 9000.0));
         return products;
     }
 }
@@ -39,5 +61,5 @@ public class FilterDemo {
 class Product {
     private int id;
     private String name;
-    private float price;
+    private double price;
 }
